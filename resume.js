64db8408f4d26ba561d79488a99662ab2e8fabcd
salviuez <107339320+salviuez@users.noreply.github.com>
2022-06-28 22:35:22 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,27 @@
+let resume = [{
+    "name" : "Arun",
+    "Mobile" : "1234567890",
+    "email" : "arun19@gmail.com"
+},
+{
+    "school" : "tagore matriculation",
+    "Group" : "computer science",
+    "percentage" : "83"
+},
+{
+    "college" : "DSCET",
+    "Department" : "computer science",
+    "percentage" : "64"
+},
+{
+    "Work Experience" : "2.6 Years",
+    "Current Designation" : "HR Recruiter",
+    "Company" : "XYZ"
+},
+{
+    "skills" : "HTML,CSS,JAVASCRIPT BASICS"
+}];
+
+for(let i in resume){
+    console.log(resume[i]);
+}
---
+++ script.js
@@ -0,0 +1,108 @@
+let array = [{
+    "id": "1",
+    "name": "Lura Senger",
+    "email": "Xander_Collier@yahoo.com"
+  },
+  {
+    "id": "2",
+    "name": "Wilburn Weber",
+    "email": "Bennett_Kreiger11@yahoo.com"
+  },
+  {
+    "id": "3",
+    "name": "Tyrique Hahn",
+    "email": "Juston.Altenwerth@yahoo.com"
+  },
+  {
+    "id": "4",
+    "name": "Lenny Bailey",
+    "email": "Guiseppe_Hegmann@yahoo.com"
+  },
+  {
+    "id": "5",
+    "name": "Vladimir Keeling",
+    "email": "Louisa_Walsh18@hotmail.com"
+  },
+  {
+    "id": "6",
+    "name": "Kellie Crona",
+    "email": "Chandler_Abernathy@yahoo.com"
+  },
+  {
+    "id": "7",
+    "name": "Carolina White",
+    "email": "Royal50@hotmail.com"
+  },
+  {
+    "id": "8",
+    "name": "Alfredo Conn",
+    "email": "Clarabelle34@hotmail.com"
+  },
+  {
+    "id": "9",
+    "name": "Stan Shanahan",
+    "email": "Lamar.McClure@hotmail.com"
+  },
+  {
+    "id": "10",
+    "name": "Marvin Fay",
+    "email": "Osbaldo58@hotmail.com"
+  },
+  {
+    "id": "11",
+    "name": "Torrance Rau",
+    "email": "Orin45@gmail.com"
+  },
+  {
+    "id": "12",
+    "name": "Harold Gutmann MD",
+    "email": "Alyce.Stracke37@yahoo.com"
+  },
+  {
+    "id": "13",
+    "name": "Taryn Torphy",
+    "email": "Dean_Breitenberg71@hotmail.com"
+  },
+  {
+    "id": "14",
+    "name": "Bryana Lang",
+    "email": "Tatum.Ullrich@yahoo.com"
+  },
+  {
+    "id": "15",
+    "name": "Nyasia Green DDS",
+    "email": "Dino83@gmail.com"
+  },
+  {
+    "id": "16",
+    "name": "Nasir Gerhold",
+    "email": "Lilian_Bashirian8@hotmail.com"
+  },
+  {
+    "id": "17",
+    "name": "Raymundo Ritchie PhD",
+    "email": "Antwan.Schoen15@yahoo.com"
+  },
+  {
+    "id": "18",
+    "name": "Delmer Marvin",
+    "email": "Kiera62@yahoo.com"
+  },
+  {
+    "id": "19",
+    "name": "Rachel Wilkinson",
+    "email": "Foster_Conroy@gmail.com"
+  },
+  {
+    "id": "20",
+    "name": "Gladys Howell",
+    "email": "Constance.Labadie10@yahoo.com"
+  }];
+
+  for(i=0; i<=7; i++){
+    console.log(array[i]);
+  }
+  for(let k in array){
+    console.log(array[k].id,array[k].name,array[k].email);
+    
+}
